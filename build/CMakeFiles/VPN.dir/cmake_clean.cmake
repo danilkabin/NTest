1c3f8d116103b2f68c9bb43893b678949947b494
danilkabin <danilkabin@proton.me>
2025-02-27 18:53:02 +0300

    Автоматическое обновление репозитория

---
+++ build/CMakeFiles/VPN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VPN.dir/link.d"
+  "CMakeFiles/VPN.dir/main.cpp.o"
+  "CMakeFiles/VPN.dir/main.cpp.o.d"
+  "VPN"
+  "VPN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VPN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
