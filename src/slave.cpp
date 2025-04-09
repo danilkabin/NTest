91fdcf332d6099647bb34867d87176ccdbb0a13f
danilkabin <danilkabin@proton.me>
2025-04-09 15:10:12 +0300

    as

--- src/slave.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/slavemanager.h"
-#include <cstring>
-
-SlaveManager::SlaveData::SlaveData() : SLAVE_PORT(0), SLAVE_ID(0) {
-  memset(&SlaveAddr, 0, sizeof(SlaveAddr));
-}
-
-SlaveManager::SlaveData::~SlaveData() {}
--- src/slavemanager.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/slavemanager.h"
-
-SlaveManager::SlaveManager() : activeThreads(0) {}
-SlaveManager::~SlaveManager() {}
-
-void SlaveManager::incrementThread() { activeThreads.fetch_add(1); }
-void SlaveManager::decrementThread() { activeThreads.fetch_sub(1); }
